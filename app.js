fc59f6a4d91182a3027dc43ff153b186eb04d5a8
Jhoan <jhoangongora71@gmail.com>
2024-11-22 10:17:34 -0500

    Cambio total carpetas

--- app.js
+++ app.js
@@ -1,39 +1,23 @@
 const express = require('express');
 const session = require('express-session');
 const flash = require('connect-flash');
-// const PgSession = require('connect-pg-simple')(session); // Para usar PostgreSQL como almacenamiento de sesiones
-const { Pool } = require('pg'); // Importar el cliente de PostgreSQL
 require('dotenv').config(); // Para cargar las variables de entorno
 
 const app = express();
 const router = require('./router');
 
-// Configuración del pool de PostgreSQL
-const pool = new Pool({
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    port: process.env.PGPORT,
-});
-
 // Configuración de middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// Configuración de sesiones para producción
-// app.use(
-//     session({
-//         store: new PgSession({
-//             pool: pool, // Usa PostgreSQL como almacenamiento de sesiones
-//             tableName: 'session', // Nombre de la tabla
-//         }),
-//         secret: 'my_secret_key',
-//         resave: false,
-//         saveUninitialized: false,
-//     })
-// );
-
+// Configuración de sesiones en memoria (no recomendado en producción, pero suficiente para pruebas)
+app.use(
+    session({
+        secret: 'my_secret_key',
+        resave: false,
+        saveUninitialized: false,
+    })
+);
 
 app.use(flash());
 
